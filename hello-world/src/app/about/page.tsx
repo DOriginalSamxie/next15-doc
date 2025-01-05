3ecf0397e7bd946c5fc7132abfd9d331929389a8
DOriginalSamxie <samadrufai@gmail.com>
2025-01-05 21:43:05 +0100

    routiing

---
+++ hello-world/src/app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About this app</h1>
+      <p>
+        This is a simple app to demonstrate how to use React with TypeScript.
+      </p>
+    </div>
+  )
+}
---
+++ hello-world/src/app/profile/page.tsx
@@ -0,0 +1,11 @@
+export default function Profile() {
+  return (
+    <div>
+      <h1>Profile</h1>
+      <p>
+        This is a simple profile page to demonstrate how to use React with
+        TypeScript.
+      </p>
+    </div>
+  )
+}
